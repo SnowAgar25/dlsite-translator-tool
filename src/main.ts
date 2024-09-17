dfde5c4502b0260d8120e1cbb9cde200f8a4523c
Tsuyumi <SnowU1310828@gmail.com>
2024-09-17 11:21:12 +0800

    refactor: 確保navLink可以立刻被注入

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { initPreviewBox } from './previewBox';
 import { addNavLink, addTranslationTableStyles } from './ui';
 
 function init(): void {
-  addNavLink('https://www.dlsite.com/maniax/works/translatable', '翻訳許可作品', 'magnifying-glass', '\\f002');
-  addNavLink('https://www.dlsite.com/translator/work', '翻訳申請', 'translate-icon', '\\f1ab');
   addTranslationTableStyles();
   initPreviewBox();
 }
 
+addNavLink('https://www.dlsite.com/maniax/works/translatable', '翻訳許可作品', 'magnifying-glass', '\\f002');
+addNavLink('https://www.dlsite.com/translator/work', '翻訳申請', 'translate-icon', '\\f1ab');
 init();
